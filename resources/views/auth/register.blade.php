680e3efc5ad7dd53047786e346a788aef7581f7f
HECTOR SAMUEL <54829965+JimTzoy@users.noreply.github.com>
2022-08-16 20:40:53 -0500

    cambiando logo wlcom

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,6 +1,7 @@
-@extends('layouts.app')
+@extends('layouts.inicio')
 
 @section('content')
+<div class="page-section">
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
@@ -73,5 +74,6 @@
             </div>
         </div>
     </div>
+</div>
 </div>
 @endsection
